b7f7b0370980bac4a6881428e0b5bf2e90d2f008
nbert71 <nicobert2205@gmail.com>
2020-11-29 17:37:54 +0100

    email ->champ unique

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,9 +7,13 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @ORM\Table(name="`user`")
+ * @UniqueEntity("email")
  */
 class User
 {
@@ -26,7 +30,8 @@ class User
     private $username;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @ORM\Column(type="string", length=255, nullable=true, unique=true)
+     * @Assert\Email
      */
     private $email;
 
