c022216c9fa9c417f4ab68ecf006d4491d6bc4aa
nbert71 <nicobert2205@gmail.com>
2020-12-12 20:38:08 +0100

    fin des fixtures plus ajout Encore

--- src/DataFixtures/FeedbackFixtures.php
+++ src/DataFixtures/FeedbackFixtures.php
@@ -17,50 +17,48 @@ class FeedbackFixtures extends Fixture
 
         $roles = array("ROLE_USER", "ROLE_ADMIN");
         $matiere = array("Chimie-GP","Mathématiques", "Ondes et Signal", "Informatique", "Mécanique");
-        $courses = array();
 
         //Creation de 4 course
         for($j = 0; $j <5; $j++) {
             $course = new Course();
             $course->setName($matiere[$j])
-                    ->setIsUE(true)
-                    ->setIsOptionGroup(false);
+                ->setIsUE(true)
+                ->setIsOptionGroup(false);
 
             $manager->persist($course);
             // $courses[$matiere[$j]] = $course;
-        }
 
-        // Creation de 5 users
-        for($i = 0; $i < 5; $i++) {
-            $user = new User();
-            $user->setFirstName($faker->firstName())
-                ->setLastName($faker->lastName());
-            $prenom = strtolower($user->getFirstName());
-            $nom = strtolower($user->getLastName());
-            $user->setUsername(substr($prenom, 0, 1) . $nom)
-                ->setEmail($user->getUsername().'@ginfo.fr')
-                ->setPassword($faker->password())
-                ->setPromo(mt_rand(2000,2020));
-            $user->setRoles([$roles[mt_rand(0, 1)]]);
 
-            //Chaque user laisse un feedback sur chaque cours
-            for($k = 0; $k < count($matiere); $k++) {
-                $fb = new Feedback();
-                $fb->setAuthor($user)
-                    ->setCourse($course)
-                    ->setTitle($faker->realText($maxNbChars = 20, $indexSize = 2))
-                    ->setComment($faker->realText($maxNbChars = 100, $indexSize = 4))
-                    ->setOverall(mt_rand(0, 5))
-                    ->setDifficulty(mt_rand(0, 5))
-                    ->setInterest(mt_rand(0, 5))
-                    ->setValid(true);
+            // Creation de 5 users
+            for ($i = 0; $i < 5; $i++) {
+                $user = new User();
+                $user->setFirstName($faker->firstName())
+                    ->setLastName($faker->lastName());
+                $prenom = strtolower($user->getFirstName());
+                $nom = strtolower($user->getLastName());
+                $user->setUsername(substr($prenom, 0, 1) . $nom)
+                    ->setEmail($user->getUsername() . '@ginfo.fr')
+                    ->setPassword($faker->password())
+                    ->setPromo(mt_rand(2000, 2020));
+                $user->setRoles([$roles[mt_rand(0, 1)]]);
 
-                $manager->persist($fb);
+                //Chaque user laisse un feedback sur chaque cours
+                for ($k = 0; $k < count($matiere); $k++) {
+                    $fb = new Feedback();
+                    $fb->setAuthor($user)
+                        ->setCourse($course)
+                        ->setTitle($faker->realText($maxNbChars = 20, $indexSize = 2))
+                        ->setComment($faker->realText($maxNbChars = 100, $indexSize = 4))
+                        ->setOverall(mt_rand(0, 5))
+                        ->setDifficulty(mt_rand(0, 5))
+                        ->setInterest(mt_rand(0, 5))
+                        ->setValid(true);
+
+                    $manager->persist($fb);
+                }
+                $manager->persist($user);
             }
-
-            $manager->persist($user);
         }
-
         $manager->flush();
     }
 }
