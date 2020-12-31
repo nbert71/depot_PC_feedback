1dc74d0373d4aa29b7aaca0627284ae17d573220
nbert71 <nicobert2205@gmail.com>
2020-12-31 16:43:03 +0100

    création form new feedback + fonction dans fbcontroller + template mais erreur request doctrine

--- src/Controller/FeedbackController.php
+++ src/Controller/FeedbackController.php
@@ -2,8 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Course;
+use App\Entity\Feedback;
+use App\Entity\User;
+use App\Form\FeedbackType;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,6 +19,7 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class FeedbackController extends AbstractController
 {
+    //page qui fait je sais pas quoi encore
     /**
      * @Route("/feedback", name="feedback")
      */
@@ -23,11 +31,55 @@ class FeedbackController extends AbstractController
 
     }
 
+    //page d'accueil
     /**
      * @Route("/home", name="home")
      */
     public function home()
     {
         return $this->render('feedback/home.html.twig');
+    }
+
+
+    //Creation d'un feedback
+
+    /**
+     * @Route("/new_feedback", name="new_feedback")
+     */
+    public function newFeedback(Request $request, User $user, EntityManagerInterface $manager)
+    {
+        $feedback = new Feedback();
+
+        $form = $this->createForm(FeedbackType::class, $feedback);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //on set l'auteur et est non modéré
+            $feedback->setValid(false);
+            $feedback->setAuthor($user);
+
+            $manager->persist($feedback);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Ton feedback a été enregistré et est en attente de modération !'
+            );
+
+            // faut rediriger vers une route qui show le cours et les autres feedback de ce cours
+            return $this->redirectToRoute('course_show', ["course_id" => $feedback->getCourse()->getId()]);
+        }
+
+        return $this->render('feedback/createFeedback.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    //Un cours avec ses feedback
+    /**
+     * @Route("/course/{course_id}", name="course_show")
+     */
+    public function courseshow(Course $course, Request $request){
+
     }
 }
